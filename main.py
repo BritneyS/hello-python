bc0d69b0fb8d1346cd809e1f5a1bc2236b5798fd
Britney Smith <brit.smith7@gmail.com>
2021-04-28 20:25:48 -0400

    Add binary serach script and call in main

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from grokkingalgorithms.scripts.binary_search import binary_search
+
+
 def print_hi(name):
     print(f'Hi, {name}')
 
 
 if __name__ == '__main__':
     print_hi('Python 🐍')
+
+    # Binary search
+    print("🔎 Binary search with a sorted list:")
+    my_sorted_list = [1, 3, 5, 7, 9]
+    print(binary_search(my_sorted_list, 7))  # if item is 7, should return position 3
